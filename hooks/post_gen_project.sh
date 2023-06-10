0b67baf437687a93113265c9f2afce4f909b88d7
Marcin Sodkiewicz <sodkiewicz.m@gmail.com>
2023-06-10 21:25:24 +0200

    Multi-module template

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
-
+echo "Elo"
 # Check if the folder exists
 if [ -d ".github" ]; then
-    mv .github ../
+    if [ ! -d "../.github/workflows" ]; then
+      echo "Creating folder ../.github/workflows"
+      mkdir -p ../.github/workflows
+      cp .github/workflows/* ../.github/workflows
+    else
+      echo "Folder ../.github/workflows already exists"
+    fi
 
-    echo "Folder moved successfully!"
+    rm -rf .github
+    echo "GitHub Workflows moved successfully"
 else
-    echo "Folder not found!"
+  echo "Folder not found!"
+fi
+
+if [ ! -d "../Makefile" ]; then
+  mv Makefile ../
 fi
