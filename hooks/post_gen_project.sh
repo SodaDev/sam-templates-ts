4165279e2c31d8731ae1dbe0c97c9680cca06014
Marcin Sodkiewicz <sodkiewicz.m@gmail.com>
2023-06-10 21:55:45 +0200

    Revert "Multi-module template"
    This reverts commit 0b67baf437687a93113265c9f2afce4f909b88d7.

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,21 +1,10 @@
 #!/bin/bash
-echo "Elo"
+
 # Check if the folder exists
 if [ -d ".github" ]; then
-    if [ ! -d "../.github/workflows" ]; then
-      echo "Creating folder ../.github/workflows"
-      mkdir -p ../.github/workflows
-      cp .github/workflows/* ../.github/workflows
-    else
-      echo "Folder ../.github/workflows already exists"
-    fi
+    mv .github ../
 
-    rm -rf .github
-    echo "GitHub Workflows moved successfully"
+    echo "Folder moved successfully!"
 else
-  echo "Folder not found!"
-fi
-
-if [ ! -d "../Makefile" ]; then
-  mv Makefile ../
+    echo "Folder not found!"
 fi
