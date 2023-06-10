b18ed190e89c35704498ca07344657f0e3431e7c
Marcin Sodkiewicz <sodkiewicz.m@gmail.com>
2023-06-10 22:03:46 +0200

    Multi-module template

--- nodejs16.x/ts-pt-github-actions/hooks/post_gen_project.sh
+++ nodejs16.x/ts-pt-github-actions/hooks/post_gen_project.sh
@@ -1,10 +1,37 @@
 #!/bin/bash
+echo "Running post_gen_project.sh"
 
 # Check if the folder exists
 if [ -d ".github" ]; then
-    mv .github ../
+    if [ ! -d "../.github/workflows" ]; then
+        echo "Creating folder ../.github/workflows"
+        mkdir -p ../.github/workflows
+        cp .github/workflows/* ../.github/workflows
+    else
+        echo "Folder ../.github/workflows already exists"
+    fi
 
-    echo "Folder moved successfully!"
+    rm -rf .github
+    echo "GitHub Workflows moved successfully"
 else
     echo "Folder not found!"
+fi
+
+if [ ! -f "../Makefile" ]; then
+    echo "Creating Makefile"
+    mv Makefile ../
+else
+    echo "Makefile already exists"
+fi
+
+if [ ! -f "../template.yaml" ]; then
+    echo "Creating rootTemplate.yaml"
+    mv rootTemplate.yaml ../template.yaml
+else
+    echo "rootTemplate.yaml already exists"
+fi
+
+if [ ! -f "../.gitignore" ]; then
+    echo "Creating root .gitIgnore"
+    mv rootGitIgnore ../.gitignore
 fi
